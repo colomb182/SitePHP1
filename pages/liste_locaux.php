21c648ce2f5b680a83ca4f36385e4f8c9131879d
colomb1982 <lauramagalypaz@hotmail.com>
2014-09-29 14:48:29 +0200

    deuxieme commit

---
+++ pages/liste_locaux.php
@@ -0,0 +1,4 @@
+<h2>Liste des locaux de l'implantation</h2>
+
+
+
---
+++ pages/liste_personnel.php
@@ -0,0 +1,9 @@
+<h2>Liste du personnel d'entretien</h2>
+<section>
+    <div>
+        <h3>Batiment F</h3>
+        <span>Julie</span>
+        <span>Rosalie</span>
+        <span>Mark</span>
+    </div> 
+</section>
